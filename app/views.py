39568e0b986292b88ebaa138018c9b8b17c8eb2b
David Ficociello <davidfic+github@gmail.com>
2014-03-29 00:48:57 -0400

    adding in dummy data to test and adding code to template to display pass/fail

--- app/views.py
+++ app/views.py
@@ -1,8 +1,36 @@
 from app import app
 from flask import render_template
+from app import collection
+
+
+
+
+
+known_good= {
+   "_id": 1,
+    "name": "John Doe",
+    "id": 2,
+    "date_of_birth": "1/2/1972",
+    "ins": "blue cross"
+}
+
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-  return render_template("index.html")
+  #db insert test
+  item_dict = {}
+  item=collection.find()
+  for i in item:
+    item_dict = i
+  equal = False
+  if item_dict == known_good:
+    print "equal"
+    equal = True
+
+  # for key,value in item_dict.iteritems():
+  #   print "key", key
+  #   print "value", value
+
+  return render_template("index.html",data=item_dict,equals=equal)
