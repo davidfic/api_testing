be34d131bcad2ff4aa7c98c7166a3f5dbb106e3b
David Ficociello <davidfic+github@gmail.com>
2014-03-29 00:48:29 -0400

    adding pymongo support

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,15 @@
 from flask import Flask
+from pymongo import MongoClient
+from config import *
+
+print "db_address is", db_address
+
+connection = MongoClient(db_address, int(db_port))
+
+db = connection["api_test"]
+collection=db['test1']
+ # MongoLab has user authentication
+db.authenticate(db_username, db_password)
 
 
 app = Flask(__name__)
